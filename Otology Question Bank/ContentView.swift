29d6ff06376ba65c5618755237539f85779a43e1
Elias <emichaelides@mac.com>
2026-02-19 20:13:35 -0600

    Initial Commit

---
+++ Otology Question Bank/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Otology Question Bank
+//
+//  Created by Elias Michaelides1 on 2/19/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Otology Question Bank/Otology_Question_BankApp.swift
@@ -0,0 +1,17 @@
+//
+//  Otology_Question_BankApp.swift
+//  Otology Question Bank
+//
+//  Created by Elias Michaelides1 on 2/19/26.
+//
+
+import SwiftUI
+
+@main
+struct Otology_Question_BankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
